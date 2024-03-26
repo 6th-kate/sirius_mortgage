1cd9e3e937b762fa14f5445d8a705fa64d595dca
Eugen Belyi <ebelyii@yandex.ru>
2024-03-27 01:13:10 +0300

    Removed dart analyze from workflow

--- lib/features/count/ui/screens/calculator/widgets/calc_button.dart
+++ lib/features/count/ui/screens/calculator/widgets/calc_button.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:sirius_mortgage/features/theme/model/theme_constants.dart';
 
-import '../../../../../../core/gen/l10n/app_localizations.g.dart';
 import '../../../../domain/form_bloc/form_bloc.dart';
 import '../../../../../locale/locale.dart';
 import '../../../../route/route.dart';
@@ -12,7 +10,6 @@ class CalculateButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final formBloc = context.read<FormBloc>();
     final isFormValid = context.watch<FormBloc>().state is ValidFormState;
 
     return SizedBox(

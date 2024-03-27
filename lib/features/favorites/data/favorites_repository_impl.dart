90fedb3302fd01dabf8143e4310fc39489d8bde1
Daniil <kononov.miromanov@gmail.com>
2024-03-27 21:27:48 +0300

    fix

--- lib/features/favorites/data/favorites_repository_impl.dart
+++ lib/features/favorites/data/favorites_repository_impl.dart
@@ -1,22 +1,28 @@
 import 'dart:convert';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:sirius_mortgage/features/calculator/domain/model/calculator_dataclass.dart';
+import 'package:sirius_mortgage/features/favorites/domain/favorites_repository.dart';
 
-class FavoritesRepository {
+class FavoritesRepository implements IFavoritesRepository {
   static const _key = 'favorites';
 
+  @override
   Future<List<SummaryInformationInput>> getAllFavorites() async {
     final prefs = await SharedPreferences.getInstance();
     final favoritesString = prefs.getString(_key) ?? '[]';
     final List<dynamic> favoritesJson = jsonDecode(favoritesString);
-    return favoritesJson.map((json) => SummaryInformationInput.fromJson(json)).toList();
+    return favoritesJson
+        .map((json) => SummaryInformationInput.fromJson(json))
+        .toList();
   }
 
+  @override
   Future<void> addFavorite(SummaryInformationInput favorite) async {
     final prefs = await SharedPreferences.getInstance();
     final List<SummaryInformationInput> favorites = await getAllFavorites();
     favorites.add(favorite);
-    final favoritesString = jsonEncode(favorites.map((e) => e.toJson()).toList());
+    final favoritesString =
+        jsonEncode(favorites.map((e) => e.toJson()).toList());
     await prefs.setString(_key, favoritesString);
   }
 }
--- lib/features/favorites/domain/model/favorites_repository.dart
+++
@@ -1,9 +0,0 @@
-import 'package:sirius_mortgage/features/calculator/domain/model/calculator_dataclass.dart';
-
-abstract interface class FavoritesRepository {
-  
-  Future<List<CalculatorResultData>> getAllFavorites();
-
-  Future<void> addFavorite(CalculatorResultData favorite);
-}
-
